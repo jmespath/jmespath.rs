0ce9af967249a12eb24f87cd5c929e0983dd43de
Michael Dowling <mtdowling@gmail.com>
2016-01-09 16:52:55 -0800

    Using enums for test assertions

--- tests/compliance.rs
+++ tests/compliance.rs
@@ -1,19 +1,86 @@
 extern crate jmespath;
 
-use std::fs;
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::Read;
 
 use jmespath::RcVar;
 use jmespath::RuntimeError;
 use jmespath::Expression;
 
-type AssertionBox = Box<TestAssertion>;
+/// Test case assertions.
+enum Assertion {
+    /// Ensures that the expression fails due to an invalid-arity error.
+    InvalidArity,
+    /// Ensures that the expression fails due to an invalid-type error.
+    InvalidType,
+    /// Ensures that the expression fails due to an invalid-value error.
+    InvalidSlice,
+    /// Ensures that the expression fails due to an unknown-function error.
+    UnknownFunction,
+    /// Ensures that an expression cannot be parsed due to a syntax error.
+    SyntaxError,
+    /// Ensures that the expression is parsed and returns an expected result.
+    ValidResult(RcVar)
+}
 
-/// Performs a test-case assertion.
-trait TestAssertion {
+impl Assertion {
     /// Runs the assertion of a test case
-    fn assert(&self, suite: &TestSuite, case: &TestCase) -> Result<(), String>;
+    fn assert(&self, suite: &TestSuite, case: &TestCase) -> Result<(), String> {
+        match self {
+            &Assertion::InvalidArity => {
+                match try!(self.try_parse(suite, case)).search(suite.given.clone()) {
+                    Err(RuntimeError::NotEnoughArguments{..}) => Ok(()),
+                    Err(RuntimeError::TooManyArguments{..}) => Ok(()),
+                    Err(e) => Err(self.err_message(suite, case, format!("{}", e))),
+                    Ok(r) => Err(self.err_message(suite, case, r.to_string().unwrap())),
+                }
+            },
+            &Assertion::InvalidType => {
+                match try!(self.try_parse(suite, case)).search(suite.given.clone()) {
+                    Err(RuntimeError::InvalidType{..}) => Ok(()),
+                    Err(RuntimeError::InvalidReturnType{..}) => Ok(()),
+                    Err(e) => Err(self.err_message(suite, case, format!("{}", e))),
+                    Ok(r) => Err(self.err_message(suite, case, r.to_string().unwrap())),
+                }
+            },
+            &Assertion::InvalidSlice => {
+                match try!(self.try_parse(suite, case)).search(suite.given.clone()) {
+                    Err(RuntimeError::InvalidSlice) => Ok(()),
+                    Err(e) => Err(self.err_message(suite, case, format!("{}", e))),
+                    Ok(r) => Err(self.err_message(suite, case, r.to_string().unwrap())),
+                }
+            },
+            &Assertion::UnknownFunction => {
+                match try!(self.try_parse(suite, case)).search(suite.given.clone()) {
+                    Err(RuntimeError::UnknownFunction{..}) => Ok(()),
+                    Err(e) => Err(self.err_message(suite, case, format!("{}", e))),
+                    Ok(r) => Err(self.err_message(suite, case, r.to_string().unwrap())),
+                }
+            },
+            &Assertion::SyntaxError => {
+                match Expression::new(&case.expression) {
+                    Err(_) => Ok(()),
+                    Ok(expr) => Err(self.err_message(suite, case, format!("Parsed {:?}", expr)))
+                }
+            },
+            &Assertion::ValidResult(ref expected_result) => {
+                match try!(self.try_parse(suite, case)).search(suite.given.clone()) {
+                    Err(e) => Err(self.err_message(suite, case, format!("{}", e))),
+                    Ok(r) => {
+                        match r.as_string() {
+                            Some(s) if s != expected_result.as_string().unwrap() => {
+                                Err(self.err_message(suite, case, r.to_string().unwrap()))
+                            },
+                            Some(_) if r != expected_result.clone() => {
+                                Err(self.err_message(suite, case, r.to_string().unwrap()))
+                            },
+                            _ => Ok(())
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     /// Attempts to parse an expression for a case, returning the expression or an error string.
     fn try_parse<'a>(&self,
@@ -34,8 +101,11 @@ trait TestAssertion {
 }
 
 struct TestSuite {
+    /// Filename of the test suite
     filename: String,
+    /// Given data of the test suite
     given: RcVar,
+    /// Collection of test cases to perform
     cases: Vec<TestCase>,
 }
 
@@ -56,9 +126,12 @@ impl TestSuite {
 }
 
 struct TestCase {
+    /// The expression being evaluated
     expression: String,
+    /// The human-readable expected result of the test-case
     expected_result: String,
-    assertion: AssertionBox
+    /// The assertion to perform for the test case
+    assertion: Assertion,
 }
 
 impl TestCase {
@@ -71,108 +144,16 @@ impl TestCase {
                 Some(err) => err.as_string().unwrap().clone()
             },
             assertion: match case.get_value("error") {
-                None => Box::new(ValidResult {
-                    expected_result: case.get_value("result").unwrap()
-                }),
+                None => Assertion::ValidResult(case.get_value("result").unwrap()),
                 Some(err) => {
                     match err.as_string().unwrap().as_ref() {
-                        "syntax" => Box::new(SyntaxError),
-                        "invalid-type" => Box::new(InvalidType),
-                        "invalid-value" => Box::new(InvalidSlice),
-                        "invalid-arity" => Box::new(InvalidArity),
-                        "unknown-function" => Box::new(UnknownFunction),
+                        "syntax" => Assertion::SyntaxError,
+                        "invalid-type" => Assertion::InvalidType,
+                        "invalid-value" => Assertion::InvalidSlice,
+                        "invalid-arity" => Assertion::InvalidArity,
+                        "unknown-function" => Assertion::UnknownFunction,
                         v @ _ => panic!("Unknown error type: {}", v),
                     }
-                }
-            }
-        }
-    }
-}
-
-/// Ensures that the expression fails due to an invalid-arity error.
-struct InvalidArity;
-
-impl TestAssertion for InvalidArity {
-    fn assert(&self, suite: &TestSuite, case: &TestCase) -> Result<(), String> {
-        match try!(self.try_parse(suite, case)).search(suite.given.clone()) {
-            Err(RuntimeError::NotEnoughArguments{..}) => Ok(()),
-            Err(RuntimeError::TooManyArguments{..}) => Ok(()),
-            Err(e) => Err(self.err_message(suite, case, format!("{}", e))),
-            Ok(r) => Err(self.err_message(suite, case, r.to_string().unwrap())),
-        }
-    }
-}
-
-/// Ensures that the expression fails due to an invalid-type error.
-struct InvalidType;
-
-impl TestAssertion for InvalidType {
-    fn assert(&self, suite: &TestSuite, case: &TestCase) -> Result<(), String> {
-        match try!(self.try_parse(suite, case)).search(suite.given.clone()) {
-            Err(RuntimeError::InvalidType{..}) => Ok(()),
-            Err(RuntimeError::InvalidReturnType{..}) => Ok(()),
-            Err(e) => Err(self.err_message(suite, case, format!("{}", e))),
-            Ok(r) => Err(self.err_message(suite, case, r.to_string().unwrap())),
-        }
-    }
-}
-
-/// Ensures that the expression fails due to an invalid-value error.
-struct InvalidSlice;
-
-impl TestAssertion for InvalidSlice {
-    fn assert(&self, suite: &TestSuite, case: &TestCase) -> Result<(), String> {
-        match try!(self.try_parse(suite, case)).search(suite.given.clone()) {
-            Err(RuntimeError::InvalidSlice) => Ok(()),
-            Err(e) => Err(self.err_message(suite, case, format!("{}", e))),
-            Ok(r) => Err(self.err_message(suite, case, r.to_string().unwrap())),
-        }
-    }
-}
-
-/// Ensures that the expression fails due to an unknown-function error.
-struct UnknownFunction;
-
-impl TestAssertion for UnknownFunction {
-    fn assert(&self, suite: &TestSuite, case: &TestCase) -> Result<(), String> {
-        match try!(self.try_parse(suite, case)).search(suite.given.clone()) {
-            Err(RuntimeError::UnknownFunction{..}) => Ok(()),
-            Err(e) => Err(self.err_message(suite, case, format!("{}", e))),
-            Ok(r) => Err(self.err_message(suite, case, r.to_string().unwrap())),
-        }
-    }
-}
-
-/// Ensures taht an expression cannot be parsed due to a syntax error.
-struct SyntaxError;
-
-impl TestAssertion for SyntaxError {
-    fn assert(&self, suite: &TestSuite, case: &TestCase) -> Result<(), String> {
-        match Expression::new(&case.expression) {
-            Err(_) => Ok(()),
-            Ok(expr) => Err(self.err_message(suite, case, format!("Parsed {:?}", expr)))
-        }
-    }
-}
-
-/// Ensures that the expression is parsed and returns an expected result.
-struct ValidResult {
-    expected_result: RcVar
-}
-
-impl TestAssertion for ValidResult {
-    fn assert(&self, suite: &TestSuite, case: &TestCase) -> Result<(), String> {
-        match try!(self.try_parse(suite, case)).search(suite.given.clone()) {
-            Err(e) => Err(self.err_message(suite, case, format!("{}", e))),
-            Ok(r) => {
-                match r.as_string() {
-                    Some(s) if s != self.expected_result.as_string().unwrap() => {
-                        Err(self.err_message(suite, case, r.to_string().unwrap()))
-                    },
-                    Some(_) if r != self.expected_result => {
-                        Err(self.err_message(suite, case, r.to_string().unwrap()))
-                    },
-                    _ => Ok(())
                 }
             }
         }
